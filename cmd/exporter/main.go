9804763ca69f19ebe9cfacdc4402fe5e94248afa
Serhii Varakuta <flood.code@gmail.com>
2019-12-04 19:25:45 +0200

    WIP

--- cmd/exporter/main.go
+++ cmd/exporter/main.go
@@ -1,5 +1,69 @@
 package main
 
-func main() {
+import (
+	"io/ioutil"
+	"log"
 
+	"github.com/prometheus/common/promlog"
+	"github.com/prometheus/common/promlog/flag"
+	"gopkg.in/alecthomas/kingpin.v2"
+
+	fcgiclient "github.com/tomasen/fcgi_client"
+)
+
+// Exporter collects OPcache status from the given FastCGI URI and exports them using
+// the prometheus metrics package.
+type Exporter struct {
+	client *fcgiclient.FCGIClient
+}
+
+func (e *Exporter) getOpcacheStatus() {
+	env := make(map[string]string)
+	env["SCRIPT_FILENAME"] = "/var/www/html/test.php"
+
+	resp, err := e.client.Get(env)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	log.Println("Status code:", resp.StatusCode)
+
+	content, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	log.Println(string(content))
+}
+
+// NewExporter returns an initialized Exporter.
+func NewExporter(uri string) (*Exporter, error) {
+	client, err := fcgiclient.Dial("tcp", uri)
+	if err != nil {
+		return nil, err
+	}
+
+	exporter := &Exporter{
+		client: client,
+	}
+
+	return exporter, nil
+}
+
+func main() {
+	var (
+		fcgiURI = kingpin.Flag("opcache.fcgi-uri", "Connection string to FastCGI server.").Default("127.0.0.1:9000").String()
+	)
+
+	promlogConfig := &promlog.Config{}
+	flag.AddFlags(kingpin.CommandLine, promlogConfig)
+	kingpin.HelpFlag.Short('h')
+	kingpin.Parse()
+
+	exporter, err := NewExporter(*fcgiURI)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	exporter.getOpcacheStatus()
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module gitlab.com/crosscone/tool/opcache-exporter
 
 go 1.13
+
+require (
+	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
+	github.com/alecthomas/units v0.0.0-20190924025748-f65c72e2690d // indirect
+	github.com/prometheus/common v0.4.1
+	github.com/tomasen/fcgi_client v0.0.0-20180423082037-2bb3d819fd19
+	gopkg.in/alecthomas/kingpin.v2 v2.2.6
+)
